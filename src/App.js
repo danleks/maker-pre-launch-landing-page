71ae124c9567cb207cb931b92264fc8844a09ed3
danleks <danilevich.lks@gmail.com>
2021-05-02 18:54:20 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import './styles/main.sass';
 import Logo from './components/Logo/Logo';
+import Hero from './components/Hero/Hero';
 
 const App = new Reef('#app', {
     template: () => {
         return `
             <div class="wrapper">
                 <nav data-component="logo"></nav>
+                <main>
+                    <my-hero data-component="hero"></my-hero>
+                    <div class="innerWrapper">
+                        <h2>other</h2>
+                    </div>
+                </main>
             </div>
         `
     }
 });
 
 
-App.attach([Logo]);
+App.attach([Logo, Hero]);
 export default App;
